8aa90feaac00cac03a516eb18fc8dbb9336dae71
venkateshbonala13 <venkateshbonala13@gmail.com>
2024-02-08 15:46:09 -0600

    customExceptions

---
+++ src/main/java/customExceptions/ReadsFile.java
@@ -0,0 +1,24 @@
+package customExceptions;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class ReadsFile {
+    public static void main(String[] args) {
+        try {
+            File myfile = new File(("C:\\Users\\venka\\OneDrive\\Desktop\\TestFile.txt"));
+
+            Scanner sc = new Scanner(myfile);
+            while (sc.hasNext()) {
+                String data = sc.nextLine();
+                System.out.println(data);
+            }
+            sc.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            System.out.println("File not found");
+            e.printStackTrace();
+        }
+    }
+}
